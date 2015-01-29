44f70941c02201374245e86b7ac6047b75867f73
Sheonna Harris <svonharris@gmail.com>
2015-01-29 17:26:25 -0500

    Trying to use YAML to create and store todo list.

--- server2.rb
+++ server2.rb
@@ -11,16 +11,30 @@ set :session_secret, "super secret"
 
 
 
-TodoList = Struct.new :task, :time
-store = YAML::Store.new ("todo_list.txt")
-
-todos = {}
-store.transaction do
-	store[:list] = {}
-end
-
  #task.new('buy milk')
  #task.complete?
+ class Task
+ 	attr_reader :task
+	def initialize(task)
+		@task = task
+		t = Time.now
+		@time = t.strftime("%l:%M%P")
+	end
+	def complete?(task)
+		
+	end
+
+end
+
+
+#YAML ONLY WORRIES ABOUT STORING EACH TASK
+#TASK ONLY WORRIES ABOUT CREATING THE TASK
+
+store = YAML::Store.new ("todo_list.yml")
+store.transaction do
+	store[:list] = [] if store[:list].nil? # Initialize array only if list doesn't exist
+end
+
 
 
 
@@ -29,16 +43,20 @@ end
 
 
 get "/" do
-	# @tasks = todos
 	store.transaction do
-		store[:list]
-	end
-	erb :todo
+        @list = store[:list]
+    end
+	erb :todo2
 end
 
 post "/added" do
-	List = Task.new	#list = Task.new
-	add_task(params[:task])		#list.add_task("cheese")
+	cat = Task.new(params[:task])
+	# store[params[:task]] = new_item
+	store.transaction do
+		store["item"] = cat
+		
+		store[:list].push(cat)
+	end
 	redirect to("/")
 end
 
