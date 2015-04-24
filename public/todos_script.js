d59d5fac552ec9e44bd2369dbafb50b2e66fddf4
Sheonna Harris <svonharris@gmail.com>
2015-04-24 15:57:02 -0400

    Added a check box

--- public/todos_script.js
+++ public/todos_script.js
@@ -1,22 +1,37 @@
 
 $( ".form-task" ).submit(function(event) {
 	// event.preventDefault();
-  $('.notepad').removeClass('none');
+  // $('.notepad').addClass('pad');
   
 });
 
-function complete (index) {
-	$('.task-item-' + index + '-container').toggleClass('complete');
-}
+$('input.js-email').on('click', function () {
+	$(this).next('.task-item-container').toggleClass('complete');
+});
+
+// $('.btn-trash').on('click', function () {
+// 	$(this)
+// });
 
 function trash (index) {
-	$('li.item-' + index).addClass('done');
+	// $('li.item-' + index).addClass('done');
 	$.post("/delete/" + index);
-	alert('done');
-	if ($('ul').is(':empty')) {
-			$('.notepad').addClass('none');
-			alert('works');
-		}
+	// $.post("/delete/" + index).done(function() {
+	// 	if ($('.task-item-0').text() == "") {
+	// 		// find();
+			// console.log('yes');
+	// 	} else {
+	// 	}
+	// });
+	// $('ul').empty();
+	// find();
+}
+function find() {
+	if ($('.task-item-0').text() == "") {
+		// $('.notepad').addClass('none');
+		console.log('yes-again');
+	} else {
+	}
 }
 
 
